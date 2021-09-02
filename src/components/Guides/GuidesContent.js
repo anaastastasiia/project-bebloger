8dbfb2c40248ab1a397ed73925d09f2566eb5402
Anastasiia <nastiaproturenko5@gmail.com>
2021-09-02 18:20:10 +0200

    added zoom

--- src/components/Guides/GuidesContent.js
+++ src/components/Guides/GuidesContent.js
@@ -5,13 +5,15 @@ const GuidesContent = (props) => {
     return (
         <>
             <div className="guid-art-first guid-art-third guid-art-second">
-                <img src={props.image} />
                 <div className="guid-art-block">
                     <div className="guid-art-content">
-                        <a className="title-categ-guides" href="">
-                            {props.categories}
-                        </a>
-                        <h2>{props.description}</h2>
+                        <div className="image-zoom">
+                            <img src={props.image} />
+                            <a className="title-categ-guides" href="">
+                                {props.categories}
+                            </a>
+                            <h2>{props.description}</h2>
+                        </div>
                         <div className="guid-art-author">
                             <img id="authors-ph" src={props.photo} />
                             <a href="">{props.name}</a>
--- src/components/Places/PlacesContent.js
+++ src/components/Places/PlacesContent.js
@@ -5,13 +5,16 @@ const PostsContent = (props) => {
     return (
         <>
             <div className="pl-art-first pl-art-second">
-                <img src={props.image} />
                 <div className="pl-art-block">
                     <div className="pl-art-content">
-                        <a className="title-categ-pleces" href="">
-                            {props.categories}
-                        </a>
-                        <h2>{props.description}</h2>
+                        <div className="image-zoom">
+                            <img src={props.image} />
+
+                            <a className="title-categ-pleces" href="">
+                                {props.categories}
+                            </a>
+                            <h2>{props.description}</h2>
+                        </div>
                         <div className="pl-art-author">
                             <img id="authors-ph" src={props.photo} />
                             <a href="">{props.name}</a>
